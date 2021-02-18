31869e0b8ccd3e73c8d410bf4d9da4205e319b68
jordan h <jordanhiatt@u.boisestate.edu>
2021-02-18 10:49:46 -0700

    error check

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-<<<<<<< HEAD
-py -m pylint main.py || pylint-exit $?
+pylint main.py || pylint-exit $?
 if [ $? -ne 0 ]; then
   echo "An error occurred while running pylint." >&2
   exit 1
+else
+  echo "no errors"
 fi
-=======
-py -m pylint main.py
->>>>>>> 60fbc7296f1d4fb8cb8dbd04962b019520da2a15
